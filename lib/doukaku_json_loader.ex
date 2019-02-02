75c8710e16190f0189aac2f3ab0cc4fa2c4df55c
Eiji MATSUMOTO <e.mattsan@gmail.com>
2019-02-02 09:32:54 +0900

    add a parser that parse JSON styled doukaku data

--- lib/doukaku_json_loader.ex
+++ lib/doukaku_json_loader.ex
@@ -1,18 +1,27 @@
 defmodule DoukakuJsonLoader do
   @moduledoc """
-  Documentation for DoukakuJsonLoader.
+  Parse JSON styled doukaku data.
   """
 
-  @doc """
-  Hello world.
+  defmodule Data, do: defstruct [:event_id, :event_url, :test_data]
+  defmodule TestData, do: defstruct [:number, :src, :expected]
 
-  ## Examples
+  alias DoukakuJsonLoader.{Data, TestData}
 
-      iex> DoukakuJsonLoader.hello()
-      :world
+  def download(url) do
+    resp = HTTPoison.get!(url)
+    resp.body
+    |> parse!()
+  end
 
-  """
-  def hello do
-    :world
+  def load_from_file(filename) do
+    filename
+    |> File.read!()
+    |> parse!()
+  end
+
+  defp parse!(data_str) do
+    data_str
+    |> Poison.decode!(as: %Data{test_data: [%TestData{}]})
   end
 end
--- test/doukaku_json_loader_test.exs
+++ test/doukaku_json_loader_test.exs
@@ -1,8 +1,4 @@
 defmodule DoukakuJsonLoaderTest do
   use ExUnit.Case
   doctest DoukakuJsonLoader
-
-  test "greets the world" do
-    assert DoukakuJsonLoader.hello() == :world
-  end
 end
